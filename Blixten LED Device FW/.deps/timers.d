4bbe76984cb4ede33827ec4bff955ccbb13c8008
Jons Persson <jons@Jonas-iMac.local>
2016-07-30 18:13:13 +0200

    Removed junk files

--- Blixten LED Device FW/.deps/timers.d
+++
@@ -1,6 +0,0 @@
-.objs/timers.o: timers.c timers.h stm32f30x.h \
- CMSIS-SP-00300-r4p5-00rel0/CMSIS/Include/core_cm4.h \
- CMSIS-SP-00300-r4p5-00rel0/CMSIS/Include/core_cmInstr.h \
- CMSIS-SP-00300-r4p5-00rel0/CMSIS/Include/cmsis_gcc.h \
- CMSIS-SP-00300-r4p5-00rel0/CMSIS/Include/core_cmFunc.h \
- CMSIS-SP-00300-r4p5-00rel0/CMSIS/Include/core_cmSimd.h
